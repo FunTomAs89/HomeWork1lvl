21dcfb2a5348e0b41e89fa500e0caa086eb956ce
FunTomAs89 <54026609+FunTomAs89@users.noreply.github.com>
2019-08-12 15:05:45 +0300

    Add files via upload

---
+++ XoGUI.java
@@ -0,0 +1,8 @@
+package geekbrains.java_level_one.lesson_eight;
+
+public class XoGUI {
+    public static void main(String[] args) {
+        new GameWindow();
+        System.out.println("exited");
+    }
+}
